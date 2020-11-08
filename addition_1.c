840bd332d40f81426f1c9ebf04d311c53b899b9b
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2020-11-08 22:23:37 +0530

    Create addition_1.c

---
+++ addition_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   
+   int var1 ;
+   int var2 ;
+   int var3 ;
+   
+   var1= 4 ;
+   var2= 3 ;
+   
+   var3 = var1 + var2 ;
+   
+   printf("result: %d", var3);
+   
+   return 0;
+}
