a73fed53e67a2217016c853471cca1f347fcf56b
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2020-11-09 22:10:46 +0530

    Create discount problem .c

---
+++ discount problem .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	float tp;
+	float da;
+	float pa;
+	float td;
+	
+	scanf("%f", &tp);
+	scanf("%f", &td);
+	
+	da= (td * tp)/100;
+	
+	pa=tp-da;s
+	printf("amount paid  %f",pa);
+	
+	return 0;
+}
