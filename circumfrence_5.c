64002bb92d051e14d2d1de905f990f773d811119
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2020-11-10 19:12:46 +0530

    Create circumfrence_5.c

---
+++ circumfrence_5.c
@@ -0,0 +1,20 @@
+// Circumference of circle
+
+#include <stdio.h>
+
+int main(void) {
+	
+	// variable declaration
+	int rad;
+	float cf;
+	
+	// initialization
+	scanf("%d", &rad);
+	
+	// operation definition
+	cf=2*3.14*rad;
+	
+	printf("circumfrence:%f",cf);
+	
+	return 0;
+}
