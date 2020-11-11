455c1d1a846ac778cb11578254a3304f2c531543
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2020-11-11 22:55:12 +0530

    Create average_9.c

---
+++ average_9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	//variable declaration
+	  float x;
+	  float y;
+	  float ave;
+	  
+	  // implementation
+	  scanf("%f",&x);
+	  scanf("%f",&y);
+	  
+	  //operation 
+	
+	  ave=(x+y)/2;
+	  
+	  printf("average:%f",ave);
+	  return 0;
+}
+
