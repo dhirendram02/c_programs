652b02f3f087c4731335896e78f3547a05859cda
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2020-11-11 22:47:35 +0530

    Create celcius to fahrenhit_8.c

---
+++ celcius to fahrenhit_8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	//variable declaration
+	  float fah;
+	  float cel;
+	  
+	  
+	  // implementation
+	  scanf("%f",&cel);
+	  
+	  
+	  //operation 
+	
+	  fah=(9/5*cel)+32;
+	  
+	  printf("fahrenhite :%f",fah);
+	  return 0;
+}
+
