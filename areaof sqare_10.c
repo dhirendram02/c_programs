78015f98434e3823e1cdc7a5ff3cffe0f0a725bc
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2020-11-12 18:08:01 +0530

    Create areaof sqare_10.c

---
+++ areaof sqare_10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	//variable declare
+	float side;
+	float are;
+	
+	
+	//initilization
+	scanf("%f ",&side);
+	
+
+	//operation defined;
+	are=side*side;
+	
+	printf("area of square %f", are);
+	return 0;
+}
+
