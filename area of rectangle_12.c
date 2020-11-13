d608dcf304431cfb9d8e2032e66b8fa442dbb8ac
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2020-11-13 11:36:42 +0530

    Create area of rectangle_12.c

---
+++ area of rectangle_12.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(void) {
+	// variable declaration 
+	float len;
+	float bret;
+	float ara;
+	
+	//initialization 
+	scanf ("%f",&len);
+	scanf ("%f",&bret);
+	
+	//operation defination
+	ara=len*bret;
+	
+	printf ("area =%f", ara);
+
+	return 0;
+}
