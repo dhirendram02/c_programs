6ff7c018a28929bdd362febddd0f2bd33c3af20e
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2020-11-15 09:24:40 +0530

    Create even or oddnumber_17.c

---
+++ even or oddnumber_17.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	int num;
+    
+    scanf("%d", &num);
+
+    
+    if(num % 2 == 0)
+    
+        printf("%d is even.", num);
+        
+    else
+        printf("%d is odd.", num);
+        
+	return 0;
+}
+
