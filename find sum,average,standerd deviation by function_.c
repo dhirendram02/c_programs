1e105354a637db51fa150a10d046f838c1f59fb2
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2021-01-01 11:13:46 +0530

    Create find sum,average,standerd deviation by function_.c

---
+++ find sum,average,standerd deviation by function_.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int sum(int);
+int aver(int);
+int std_deviati(int);
+int main()
+{
+    int a,b,c,d,e,x,y,z;
+    scanf ("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    
+    x= sum( x);
+   y= aver( y);
+   z= std_deviati( z);
+    
+    printf ("%d",x);
+    printf ("%d",y);
+    printf ("%d",z);
+    
+    return 0;
+}
+ sum(int a,int b,int c,int d,int e)
+ aver(int a,int b,int c,int d,int e)
+ std_deviati(int a,int b,int c,int d,int e)
+ {
+     int x,y,z,g;
+     sum
+     {
+         x=a+b+c+d+e;
+         printf ("%d",x);
+         
+     }
+     aver
+     {
+         y=(a+b+c+d+e)/5;
+         printf("%d",y);
+     }
+     std_deviati
+     {
+         g=(a+b+c+d+e)/5;
+         z=((a-g)+(b-g)+(c-g)+(d-g)+(e-g))/5-1;
+     }
+     return 0;
+ }
+
