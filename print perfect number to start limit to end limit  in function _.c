6e9a3cc0b6e5e01a40361b53b6aa8afece47893f
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2021-01-24 11:06:55 +0530

    Create print perfect number to start limit to end limit  in function _.c

---
+++ print perfect number to start limit to end limit  in function _.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+ 
+int checkPerfect(int n1);
+int PerfectNum(int stLimit, int enLimit);
+
+int main()
+{
+    int stLimit, enLimit;
+	
+    scanf("%d", &stLimit);
+    
+    scanf("%d", &enLimit);
+     
+    printf("The perfect numbers between %d to %d are :", stLimit, enLimit);
+    PerfectNum(stLimit, enLimit);
+    
+    return 0;
+}
+
+int checkPerfect(int n1)
+{
+    int i, sum;
+     
+    sum = 0;
+    for(i=1; i<n1; i++)
+    {
+        if(n1 % i == 0)
+        {
+            sum += i;
+        }
+    }
+    if(sum == n1)
+        return 1;
+    else
+        return 0;
+}
+
+int PerfectNum(int stLimit, int enLimit)
+{
+    
+    while(stLimit <= enLimit)
+    {
+        if(checkPerfect(stLimit))
+        {
+            printf(" %d  ", stLimit);
+        }
+        stLimit++;
+    }   
+}
