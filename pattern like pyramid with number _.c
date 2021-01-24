7e76f6f2b798b14d0eb8b2ea42814da81dc51db6
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2021-01-24 10:30:24 +0530

    Create pattern like pyramid with number _.c

---
+++ pattern like pyramid with number _.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int i,j,spc,row,k,t=1;
+	scanf("%d",&row);
+	spc=row+4-1;
+	for(i=1; i<=row;i++)
+	{
+	    for(k=spc; k>=1; k--)
+	    {
+	        printf(" ");
+	    }
+	    for(j=1;j<=i;j++)
+	    printf("%d", t++);
+	    printf("\n");
+	    spc--;
+	    
+	}
+	return 0;
+}
+
