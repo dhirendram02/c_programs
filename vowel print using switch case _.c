80fd77de976320cf7419ce4a929d65130209d192
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2021-01-26 18:23:51 +0530

    Create vowel print using switch case _.c

---
+++ vowel print using switch case _.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int ch ;
+	scanf("%d",&ch);
+	switch(ch)
+	{
+	    case 1: printf("a");
+	    break;
+	    case 2: printf("e");
+	    break;
+	    case 3: printf("i");
+	    break;
+	    case 4: printf("o");
+	    break;
+	    case 5: printf("u");
+	    break;
+	    default : printf("enter wrong choice");
+	    break;
+	}
+	return 0;
+}
+
