001084d55757ec6c163f4456ef32c47712fbf74a
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2021-02-17 08:39:03 +0530

    Create subtraction in 2D array_93.c

---
+++ subtraction in 2D array_93.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main()
+{
+  int arr1[50][50],arr2[50][50],arr3[50][50],i,j,n;
+  
+      
+       scanf("%d", &n); 
+  
+    
+       
+       for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+	           printf("element - [%d],[%d] : ",i,j);
+	           scanf("%d",&arr1[i][j]);
+            }
+        }   
+  
+       
+       for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+	           printf("element - [%d],[%d] : ",i,j);
+	           scanf("%d",&arr2[i][j]);
+            }
+        }    
+   
+  for(i=0;i<n;i++)
+    {
+      printf("\n");
+      for(j=0;j<n;j++)
+           printf("%d\t",arr1[i][j]);
+    }
+  
+  printf("\n");
+  for(i=0;i<n;i++)
+    {
+      printf("\n");
+      for(j=0;j<n;j++)
+      printf("%d\t",arr2[i][j]);
+    }
+   printf("\n");
+   for(i=0;i<n;i++)
+       for(j=0;j<n;j++)
+            arr3[i][j]=arr1[i][j]-arr2[i][j];
+   printf("\n");
+   for(i=0;i<n;i++){
+       printf("\n");
+       for(j=0;j<n;j++)
+            printf("%d\t",arr3[i][j]);
+   }
+   printf("\n\n");
+   return 0;
+}
