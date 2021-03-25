578badb5a73fdb226f6d3da38ad4b2a97bd09645
Dhirendra Mishra <74134523+dhirendram02@users.noreply.github.com>
2021-03-25 18:02:54 +0530

    Create Fibonacci series _101.c

---
+++ Fibonacci series _101.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int fibo(int i)
+{
+    if(i==0)
+    {
+    return 0;
+    }
+    if(i==1)
+    {
+    return 1;
+    }
+    return  (fibo(i-1)+ fibo(i-2));
+    }
+
+ int main()
+     {
+         int i;
+         for(i=0;i<10;i++)
+         {
+            printf("%d",fibo(i)); 
+         }
+         return 0;
+     }
